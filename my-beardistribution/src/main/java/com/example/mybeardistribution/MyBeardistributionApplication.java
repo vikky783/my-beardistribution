66f2be31489e20717638de3b68cc14bb1c70f1d7
vikkykumar <vikkykumar@192.168.0.3>
2019-09-21 16:04:27 +0530

    new

---
+++ my-beardistribution/src/main/java/com/example/mybeardistribution/MyBeardistributionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybeardistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBeardistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBeardistributionApplication.class, args);
+	}
+
+}
